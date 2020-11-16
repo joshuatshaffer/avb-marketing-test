1c5304f4f9fe752174d7ace590c0f0708d9b8a55
joshuatshaffer <joshuatshaffer@gmail.com>
2020-11-16 17:57:05 -0600

    Removed unneeded things from app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { ContactList } from "./ContactList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ContactList
-        selectContact={contactId => alert(`You selected contact ${contactId}.`)}
-      />
-    </div>
+    <ContactList
+      selectContact={contactId => alert(`You selected contact ${contactId}.`)}
+    />
   );
 }
 
