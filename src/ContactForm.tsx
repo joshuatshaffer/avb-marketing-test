b853f2b8f18b4fd9e92b8843b5179cd10c0043c2
joshuatshaffer <joshuatshaffer@gmail.com>
2020-11-17 11:32:04 -0600

    Added api helpers and form to add, edit, and delete contacts. Does not support emails right now.

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { ContactResponseDto } from "./avb-contacts-api";
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  ContactResponseDto,
+  ContactDto,
+  addContact,
+  editContact,
+  deleteContact
+} from "./avb-contacts-api";
 
 export interface ContactFormProps {
   /** The contact to edit. If this is not defined the form creates a new contact. */
@@ -10,11 +17,43 @@ export interface ContactFormProps {
 
 /** This component serves to both create, view, and edit contacts. */
 export function ContactForm({ contactToEdit, onFormDone }: ContactFormProps) {
-  return (
-    <form>
-      <div>{JSON.stringify({ contactToEdit })}</div>
+  const { register, handleSubmit } = useForm<ContactDto>({
+    defaultValues: contactToEdit ?? { firstName: "", lastName: "", emails: [] }
+  });
 
+  const onSubmit: SubmitHandler<ContactDto> = data => {
+    if (contactToEdit) {
+      editContact({ ...data, id: contactToEdit.id });
+    } else {
+      addContact(data);
+    }
+    onFormDone?.();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        First Name
+        <input name="firstName" ref={register({ required: true })} />
+      </label>
+
+      <label>
+        Last Name
+        <input name="lastName" ref={register({ required: true })} />
+      </label>
+
+      {contactToEdit ? (
+        <button
+          onClick={() => {
+            deleteContact(contactToEdit);
+            onFormDone?.();
+          }}
+        >
+          Delete
+        </button>
+      ) : null}
       <button onClick={() => onFormDone?.()}>Cancel</button>
+      <input type="submit" value="Submit" />
     </form>
   );
 }
--- src/avb-contacts-api.ts
+++ src/avb-contacts-api.ts
@@ -23,6 +23,21 @@ export interface ContactDto {
 
 const apiRootUrl = "https://avb-contacts-api.herokuapp.com";
 
+export function addContact(contact: ContactDto) {
+  return axios.post<ContactResponseDto>(`${apiRootUrl}/contacts`, contact);
+}
+
+export function editContact({ id: contactId, ...contact }: ContactResponseDto) {
+  return axios.put<ContactResponseDto>(
+    `${apiRootUrl}/contacts/${contactId}`,
+    contact
+  );
+}
+
+export function deleteContact({ id: contactId }: ContactResponseDto) {
+  return axios.delete<unknown>(`${apiRootUrl}/contacts/${contactId}`);
+}
+
 export function useContactsPaginated() {
   const { data, error } = useSWR<ContactResponsePaginatedDto>(
     `${apiRootUrl}/contacts/paginated`,
